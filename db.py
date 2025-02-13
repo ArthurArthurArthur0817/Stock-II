2822d708dd1ea27308ac5df6c70b633356c8591b
Yu-Chien Huang <102396327+ArthurArthurArthur0817@users.noreply.github.com>
2025-02-13 15:00:54 +0800

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Ab271642",
+        password="",
         database="my_database",
         port=3307
     )
