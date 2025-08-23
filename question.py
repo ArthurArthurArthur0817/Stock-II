0563751f5e12f5280f319ab3db2d84f1ea825fa9
Yu-Chien Huang <102396327+ArthurArthurArthur0817@users.noreply.github.com>
2025-08-23 18:46:36 +0800

    Add files via upload

---
+++ question.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai
+import re
+
+# 配置 Gemini API 金鑰
+api_key = 'AIzaSyBMIXMv2ewbi-IJidv4lCwPXXyOdBzchUQ'
+genai.configure(api_key=api_key)
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-2.0-flash-exp')
+    response = model.generate_content(question)
+    response.resolve()
+
+    # 格式化文字
+    formatted_text = re.sub(r'\*+', '', response.text)
+    formatted_text = formatted_text.replace('•', '・')
+    return formatted_text
